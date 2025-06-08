086e36093a1a32fc1fb98c0a5e719dc3b454918d
GosuCode <gosucode1945@gmail.com>
2025-06-08 14:26:38 +0545

    feat: add hero section

--- app/(root)/page.js
+++ app/(root)/page.js
@@ -1,11 +1,13 @@
 import { Footer } from "@/components/layout/footer";
 import { Header } from "@/components/layout/header";
+import Hero from "@/components/layout/sections/hero";
 import React from "react";
 
 const page = () => {
   return (
     <>
       <Header />
+      <Hero />
       <Footer />
     </>
   );
