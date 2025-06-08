654578411f72906603ceb029ac449b9ddae12461
bimal009 <pandeybimal616@gmail.com>
2025-06-08 17:43:05 +0545

    feat: enhance MongoDB connection handling

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,4 @@
+import { QueryProvider } from "@/lib/providers/QueryProvider";
 import "./globals.css";
 
 
@@ -9,10 +10,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-      >
-        {children}
-      </body>
+        <body>
+      <QueryProvider>
+          {children}
+      </QueryProvider>
+        </body>
     </html>
   );
 }
